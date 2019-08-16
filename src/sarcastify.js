45ea2042f0bd49add59a94745f049dfc0ceba854
Ben Heil <ben.heil@ZIRMED.COM>
2019-08-16 11:09:24 -0400

    initial commit

---
+++ src/sarcastify.js
@@ -0,0 +1,10 @@
+export function sarcastify(str){
+    let chars = [...str];
+    let pattern = /^[A-Za-z]$/;
+    for(var i in chars){
+        if(pattern.test(chars[i])){
+            chars[i] = i % 2 === 0 ? chars[i].toUpperCase() : chars[i].toLowerCase();
+        }
+    }
+    return chars.join('');
+}
