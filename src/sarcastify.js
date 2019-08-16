c693fe227e809fdfbe3c8e4b7c9472a4c41b611f
Ben Heil <ben.heil@ZIRMED.COM>
2019-08-16 11:10:58 -0400

    change order

--- src/sarcastify.js
+++ src/sarcastify.js
@@ -3,7 +3,7 @@ export function sarcastify(str){
     let pattern = /^[A-Za-z]$/;
     for(var i in chars){
         if(pattern.test(chars[i])){
-            chars[i] = i % 2 === 0 ? chars[i].toUpperCase() : chars[i].toLowerCase();
+            chars[i] = i % 2 !== 0 ? chars[i].toUpperCase() : chars[i].toLowerCase();
         }
     }
     return chars.join('');
